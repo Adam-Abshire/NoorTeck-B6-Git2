5fd08752b1dfe7ff4f2b18f5c829aeae68a13ada
Adam-Abshire <71105578+Adam-Abshire@users.noreply.github.com>
2022-05-05 20:29:43 -0400

    Added Subtract Method

--- src/test/java/git2/DemoTwo.java
+++ src/test/java/git2/DemoTwo.java
@@ -5,10 +5,15 @@ public class DemoTwo {
 	public static void main(String[] args) {
 		System.out.println("Git demo 2....");
 		add(1, 2);
+		subtract(3, 2);
 	}
 	
 	public static void add(int a, int b) {
 		System.out.println(a + " + " + b + " = " + (a + b));
+	}
+	
+	public static void subtract(int a, int b) {
+		System.out.println(a + " - " + b + " = " + (a-b));
 	}
 
 }
