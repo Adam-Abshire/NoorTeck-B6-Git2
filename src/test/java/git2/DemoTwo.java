a598f69b224f9504de728c596e1223cdf75e32e1
Adam-Abshire <71105578+Adam-Abshire@users.noreply.github.com>
2022-05-05 20:22:56 -0400

    Implemented Add Method

--- src/test/java/git2/DemoTwo.java
+++ src/test/java/git2/DemoTwo.java
@@ -4,6 +4,11 @@ public class DemoTwo {
 	
 	public static void main(String[] args) {
 		System.out.println("Git demo 2....");
+		add(1, 2);
+	}
+	
+	public static void add(int a, int b) {
+		System.out.println(a + " + " + b + " = " + (a + b));
 	}
 
 }
