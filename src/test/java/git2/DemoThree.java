9273aacec5cf22dd734262154fa56acd129710bf
Adam-Abshire <71105578+Adam-Abshire@users.noreply.github.com>
2022-05-05 21:20:59 -0400

    added methods to demo3 and demo4

--- src/test/java/git2/DemoThree.java
+++ src/test/java/git2/DemoThree.java
@@ -4,6 +4,12 @@ public class DemoThree {
 	
 	public static void main(String[] args) {
 		System.out.println("Demo Three..");
+		subtract(5, 2);
+	}
+	
+	public static void subtract(int a, int b) {
+		int result = a - b;
+		System.out.println(a + " - " + b + " = " + result);
 	}
 
 }
